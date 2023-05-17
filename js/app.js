2d9c8b5d8ccafc26e71713282db5cdd2e8cea55f
zejenz <131663597+zejenz@users.noreply.github.com>
2023-05-17 18:19:42 +0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(function() {
+
+   $("[data-scroll]").on("click", function(event){
+event.preventDefault();
+
+       var blockId = $(this).data('scroll'),
+           blockOffset = $(blockId).offset().top;
+
+       $("html, body").animate({
+           scrollTop: blockOffset}, 500);
+       });
+
+    });
